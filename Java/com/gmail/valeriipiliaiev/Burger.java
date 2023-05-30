c150b508d7d241100c8aa689ff24d7bcac4cbdcd
Valerii Piliaiev <133023422+VPiliaiev@users.noreply.github.com>
2023-05-30 23:37:06 +0300

    Created 3 Burgers with description

---
+++ Java/com/gmail/valeriipiliaiev/Burger.java
@@ -0,0 +1,43 @@
+package com.gmail.valeriipiliaiev;
+public class Burger {
+    private String bun;
+    private String meat;
+    private String cheese;
+    private String greens;
+    private String mayo;
+
+    public Burger(String bun, String meat, String cheese, String greens, String mayo) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayo = mayo;
+        System.out.println("Звичайний бургер: " + bun + ", " + meat + ", " + cheese + ", " + greens + ", " + mayo);
+    }
+    public Burger(String bun, String meat, String cheese, String greens) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayo = "";
+        System.out.println("Дієтичний бургер: " + bun + ", " + meat + ", " + cheese + ", " + greens);
+    }
+    public Burger(String bun, String meat, String cheese, String greens, String mayo, int meatCount) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayo = mayo;
+        if (meatCount == 2) {
+            System.out.println("Бургер з подвійним м'ясом: " + bun + ", " + meat + ", " + cheese + ", " + greens + ", " + mayo);
+        } else {
+            System.out.println("Бургер з подвійним м'ясом може включати тільки 2 куска м'яса");
+        }
+    }
+
+    public static void main(String[] args) {
+        Burger firstBurger = new Burger("Булочка", "М'ясо", "Сир ", "Салат", "Майонез");
+        Burger secondBurger = new Burger("Булочка", "М'ясо", "Сир ", "Салат");
+        Burger thirdBurger = new Burger("Булочка", "М'ясо", "Сир ", "Салат", "Майонез", 2);
+    }
+}
